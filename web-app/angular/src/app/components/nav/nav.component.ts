9467126490e0937fc4889503552e845168c80b15
terrybjr <bkterryjr@gmail.com>
2024-11-23 12:30:23 -0600

    commit

--- web-app/angular/src/app/components/nav/nav.component.ts
+++ web-app/angular/src/app/components/nav/nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav2',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+    selector: 'app-nav2',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.css'],
+    standalone: false
 })
 export class NavComponent {
 
