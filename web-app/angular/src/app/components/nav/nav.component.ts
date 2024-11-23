2e7ae6c3b2e80a37d2ef8aa2798fb39738794caf
terrybjr <bkterryjr@gmail.com>
2024-11-23 12:32:27 -0600

    Revert "commit"
    This reverts commit 9467126490e0937fc4889503552e845168c80b15.

--- web-app/angular/src/app/components/nav/nav.component.ts
+++ web-app/angular/src/app/components/nav/nav.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-nav2',
-    templateUrl: './nav.component.html',
-    styleUrls: ['./nav.component.css'],
-    standalone: false
+  selector: 'app-nav2',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
 
